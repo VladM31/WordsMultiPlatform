c16a528b6dee053857352160fe665d775f02f7ff
Vlad <laft921@gmail.com>
2025-11-01 19:48:22 +0200

    Refactor ExerciseSelectAction to use sealed interface instead of sealed class for improved type safety and extensibility

--- composeApp/src/commonMain/kotlin/vm/words/ua/exercise/ui/actions/ExerciseSelectAction.kt
+++ composeApp/src/commonMain/kotlin/vm/words/ua/exercise/ui/actions/ExerciseSelectAction.kt
@@ -3,15 +3,15 @@ package vm.words.ua.exercise.ui.actions
 import vm.words.ua.exercise.domain.models.enums.Exercise
 import vm.words.ua.words.domain.models.UserWord
 
-sealed class ExerciseSelectAction {
+sealed interface ExerciseSelectAction {
     data class Init(
         val playListId: String?,
         val words: List<UserWord>,
         val transactionId: String,
-    ) : ExerciseSelectAction()
+    ) : ExerciseSelectAction
 
-    data class AddExercise(val exercise: Exercise) : ExerciseSelectAction()
-    data class RemoveExercise(val exercise: Exercise) : ExerciseSelectAction()
-    object ConfirmSelection : ExerciseSelectAction()
+    data class AddExercise(val exercise: Exercise) : ExerciseSelectAction
+    data class RemoveExercise(val exercise: Exercise) : ExerciseSelectAction
+    object ConfirmSelection : ExerciseSelectAction
 }
 
