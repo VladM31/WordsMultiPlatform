790f89997038b5b3398d458672d0e70d44280f94
Vlad <laft921@gmail.com>
2025-11-10 19:52:05 +0200

    Add InstructionViewModel for handling PDF instructions

---
+++ composeApp/src/commonMain/kotlin/vm/words/ua/settings/ui/vm/InstructionViewModel.kt
@@ -0,0 +1,8 @@
+package vm.words.ua.settings.ui.vm
+
+import vm.words.ua.core.config.AppRemoteConfig
+import vm.words.ua.core.domain.managers.ByteContentManager
+
+class InstructionViewModel(
+    byteContentManager: ByteContentManager
+) : PdfViewerViewModel(byteContentManager, { AppRemoteConfig.instructionLink })
