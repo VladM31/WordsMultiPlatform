9128dc0effb654ffd3b97e0d93587580cd2af23f
Vlad <laft921@gmail.com>
2025-11-10 15:20:16 +0200

    Add PdfViewState data class to manage PDF content and error states

---
+++ composeApp/src/commonMain/kotlin/vm/words/ua/settings/ui/states/PdfViewState.kt
@@ -0,0 +1,10 @@
+package vm.words.ua.settings.ui.states
+
+import vm.words.ua.core.domain.models.ByteContent
+import vm.words.ua.core.ui.models.ErrorMessage
+import vm.words.ua.core.ui.states.ErrorableState
+
+data class PdfViewState(
+    val content: ByteContent? = null,
+    override val errorMessage: ErrorMessage? = null
+) : ErrorableState
