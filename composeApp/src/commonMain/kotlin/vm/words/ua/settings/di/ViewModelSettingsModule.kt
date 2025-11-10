01e005c200428d2e58ed8aa29f02d85d802a99bf
Vlad <laft921@gmail.com>
2025-11-10 19:51:59 +0200

    Add InstructionViewModel binding to settings module

--- composeApp/src/commonMain/kotlin/vm/words/ua/settings/di/ViewModelSettingsModule.kt
+++ composeApp/src/commonMain/kotlin/vm/words/ua/settings/di/ViewModelSettingsModule.kt
@@ -4,11 +4,18 @@ import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.factory
 import org.kodein.di.instance
+import vm.words.ua.settings.ui.vm.InstructionViewModel
 import vm.words.ua.settings.ui.vm.PolicyViewModel
 
 internal val viewModelSettingsModule = DI.Module("viewModelSettingsModule") {
     bind<PolicyViewModel>() with factory {
         PolicyViewModel(
+            byteContentManager = instance()
+        )
+    }
+
+    bind<InstructionViewModel>() with factory {
+        InstructionViewModel(
             byteContentManager = instance()
         )
     }
