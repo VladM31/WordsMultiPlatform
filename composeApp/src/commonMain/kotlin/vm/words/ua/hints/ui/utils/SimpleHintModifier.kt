5f9ee8a281189ee4ed01b526397c7c01c8913b76
Vlad <laft921@gmail.com>
2025-11-23 21:21:23 +0200

    Add SimpleHintArgs interface and overloads for viewHint modifier to support argument-based hint display

--- composeApp/src/commonMain/kotlin/vm/words/ua/hints/ui/utils/SimpleHintModifier.kt
+++ composeApp/src/commonMain/kotlin/vm/words/ua/hints/ui/utils/SimpleHintModifier.kt
@@ -8,8 +8,35 @@ import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
 import vm.words.ua.hints.domain.models.HintPosition
 
+interface SimpleHintArgs {
+    val text: String
+    val position: HintPosition
+}
 
-fun Modifier.simpleHint(
+fun Modifier.viewHint(
+    args: SimpleHintArgs,
+    current: SimpleHintArgs,
+): Modifier {
+    return viewHint(
+        text = args.text,
+        position = args.position,
+        visible = current == args,
+    )
+}
+
+fun Modifier.viewHint(
+    args: SimpleHintArgs,
+    visible: Boolean,
+): Modifier {
+    return viewHint(
+        text = args.text,
+        position = args.position,
+        visible = visible,
+    )
+}
+
+
+fun Modifier.viewHint(
     text: String,
     position: HintPosition,
     visible: Boolean,
