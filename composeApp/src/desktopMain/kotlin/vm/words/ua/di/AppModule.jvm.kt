cd552fce473e8aafd29234fc598d9b4291d59502
Vlad <laft921@gmail.com>
2025-12-06 21:56:07 +0200

    Implement ViewModel lifecycle management for iOS, JS, JVM, and WasmJS targets

--- composeApp/src/desktopMain/kotlin/vm/words/ua/di/AppModule.jvm.kt
+++ composeApp/src/desktopMain/kotlin/vm/words/ua/di/AppModule.jvm.kt
@@ -2,17 +2,45 @@ package vm.words.ua.di
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.kodein.di.direct
 import org.kodein.di.instance
+import vm.words.ua.navigation.SimpleNavController
+import kotlin.reflect.KClass
 
 /**
- * JVM/Desktop implementation using simple remember
- * Desktop apps don't have configuration changes like Android
+ * JVM/Desktop implementation using ViewModelStoreOwner and ViewModelProvider.Factory
+ * Proper ViewModel lifecycle management per route
  */
 @Composable
 actual inline fun <reified T : Any> rememberInstance(): T {
-    return remember {
-        DiContainer.di.direct.instance<T>()
+    return if (isViewModel(T::class)) {
+        val navController: SimpleNavController = DiContainer.di.direct.instance()
+        @Suppress("UNCHECKED_CAST")
+        viewModel<ViewModel>(
+            viewModelStoreOwner = navController.viewModelStoreOwner(),
+            modelClass = T::class as KClass<ViewModel>,
+            factory = object : ViewModelProvider.Factory {
+                @Suppress("UNCHECKED_CAST")
+                override fun <VM : ViewModel> create(
+                    modelClass: KClass<VM>,
+                    extras: androidx.lifecycle.viewmodel.CreationExtras
+                ): VM {
+                    return DiContainer.di.direct.instance<T>() as VM
+                }
+            }
+        ) as T
+    } else {
+        remember {
+            DiContainer.di.direct.instance<T>()
+        }
     }
+}
+
+@PublishedApi
+internal fun <T : Any> isViewModel(kClass: KClass<T>): Boolean {
+    return ViewModel::class.java.isAssignableFrom(kClass.java)
 }
 
--- composeApp/src/jsMain/kotlin/vm/words/ua/di/AppModule.js.kt
+++ composeApp/src/jsMain/kotlin/vm/words/ua/di/AppModule.js.kt
@@ -2,17 +2,47 @@ package vm.words.ua.di
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.kodein.di.direct
 import org.kodein.di.instance
+import vm.words.ua.navigation.SimpleNavController
+import kotlin.reflect.KClass
 
 /**
- * JS/Web implementation using simple remember
- * Web apps don't have configuration changes like Android
+ * JS/Web implementation using ViewModelStoreOwner and ViewModelProvider.Factory
+ * Proper ViewModel lifecycle management per route
  */
 @Composable
 actual inline fun <reified T : Any> rememberInstance(): T {
-    return remember {
-        DiContainer.di.direct.instance<T>()
+    return if (isViewModel<T>()) {
+        val navController: SimpleNavController = DiContainer.di.direct.instance()
+        @Suppress("UNCHECKED_CAST")
+        viewModel<ViewModel>(
+            viewModelStoreOwner = navController.viewModelStoreOwner(),
+            modelClass = T::class as KClass<ViewModel>,
+            factory = object : ViewModelProvider.Factory {
+                @Suppress("UNCHECKED_CAST")
+                override fun <VM : ViewModel> create(
+                    modelClass: KClass<VM>,
+                    extras: androidx.lifecycle.viewmodel.CreationExtras
+                ): VM {
+                    return DiContainer.di.direct.instance<T>() as VM
+                }
+            }
+        ) as T
+    } else {
+        remember {
+            DiContainer.di.direct.instance<T>()
+        }
     }
+}
+
+@PublishedApi
+internal inline fun <reified T : Any> isViewModel(): Boolean {
+    // For JS, check simple name contains ViewModel pattern
+    val simpleName = T::class.simpleName ?: return false
+    return simpleName.endsWith("ViewModel") || simpleName == "ViewModel"
 }
 
