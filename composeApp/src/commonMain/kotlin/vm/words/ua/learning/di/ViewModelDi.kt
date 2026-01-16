2f1a7cd15be4e18dd31f9272de2a464a8cc39f0a
Vlad <laft921@gmail.com>
2026-01-16 20:09:18 +0200

    Refactor LearningViewModelDi: change binding from singleton to provider for LearningPlanVm

--- composeApp/src/commonMain/kotlin/vm/words/ua/learning/di/ViewModelDi.kt
+++ composeApp/src/commonMain/kotlin/vm/words/ua/learning/di/ViewModelDi.kt
@@ -3,11 +3,11 @@ package vm.words.ua.learning.di
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.instance
-import org.kodein.di.singleton
+import org.kodein.di.provider
 import vm.words.ua.learning.ui.vms.LearningPlanVm
 
 internal val learningViewModelDi = DI.Module("LearningViewModelDi") {
-    bind<LearningPlanVm>() with singleton {
+    bind<LearningPlanVm>() with provider {
         LearningPlanVm(
             learningHistoryManager = instance(),
             learningPlanManager = instance()
