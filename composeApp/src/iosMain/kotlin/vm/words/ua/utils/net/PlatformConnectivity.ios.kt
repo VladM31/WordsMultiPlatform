5b9a5f1ce6dd5e1e40c3539261db6c52b8746702
Vlad <laft921@gmail.com>
2026-02-15 01:25:27 +0200

    Add HTTP-based connectivity support in createConnectivity function for iOS

--- composeApp/src/iosMain/kotlin/vm/words/ua/utils/net/PlatformConnectivity.ios.kt
+++ composeApp/src/iosMain/kotlin/vm/words/ua/utils/net/PlatformConnectivity.ios.kt
@@ -1,13 +1,18 @@
 package vm.words.ua.utils.net
 
 import dev.jordond.connectivity.Connectivity
-import kotlin.time.Duration.Companion.seconds
+import io.ktor.client.*
+import io.ktor.client.engine.darwin.*
 
-actual fun createConnectivity(): Connectivity =
-    Connectivity {
+actual fun createConnectivity(): Connectivity {
+    // Explicitly create HttpClient to use HTTP-based connectivity (not device-based)
+    val httpClient = HttpClient(Darwin)
+
+    return Connectivity(httpClient = httpClient) {
         autoStart = true
         urls("cloudflare.com", "study-words.com")
         port = 443
         pollingIntervalMs = 15.seconds
         timeoutMs = 5.seconds
     }
+}
