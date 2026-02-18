165c0a6086dff931a2931a52b257b533d73acec1
Vlad <laft921@gmail.com>
2026-02-18 23:10:48 +0200

    Refactor GoogleSignUpScreen and TelegramSignUpScreen; update icon handling and streamline layout structure

--- composeApp/src/commonMain/kotlin/vm/words/ua/auth/ui/screen/GoogleSignUpScreen.kt
+++ composeApp/src/commonMain/kotlin/vm/words/ua/auth/ui/screen/GoogleSignUpScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,22 +19,22 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.jetbrains.compose.resources.painterResource
 import vm.words.ua.auth.ui.actions.GoogleSignUpAction
+import vm.words.ua.auth.ui.hints.GoogleSignUpScreenHintController
 import vm.words.ua.auth.ui.hints.GoogleSignUpScreenHintStep
 import vm.words.ua.auth.ui.hints.createGoogleSignUpScreenHintController
+import vm.words.ua.auth.ui.states.GoogleSignUpState
 import vm.words.ua.auth.ui.vms.GoogleSignUpViewModel
 import vm.words.ua.core.domain.models.enums.Currency
 import vm.words.ua.core.ui.AppTheme
 import vm.words.ua.core.ui.components.*
 import vm.words.ua.core.utils.isNotPhoneFormat
+import vm.words.ua.core.utils.rememberInterfaceMaxWidth
 import vm.words.ua.di.rememberInstance
 import vm.words.ua.navigation.Screen
 import vm.words.ua.navigation.SimpleNavController
 import vm.words.ua.utils.hints.ui.components.SimpleHintHost
 import vm.words.ua.utils.hints.ui.utils.viewHint
-import wordsmultiplatform.composeapp.generated.resources.Res
-import wordsmultiplatform.composeapp.generated.resources.info_in_circle
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -68,7 +70,7 @@ fun GoogleSignUpScreen(
                 showBackButton = true,
                 onBackClick = { navController.popBackStack() },
                 showAdditionalButton = true,
-                additionalButtonImage = painterResource(Res.drawable.info_in_circle),
+                additionalButtonVector = Icons.Filled.Info,
                 onAdditionalClick = {
                     navController.navigate(Screen.Policy)
                 },
@@ -76,108 +78,119 @@ fun GoogleSignUpScreen(
                 additionalButtonStepHint = GoogleSignUpScreenHintStep.PRIVACY_POLICY
             )
 
-            val columns = if (isNotPhoneFormat()) 2 else 1
-
-            Column(modifier = Modifier.fillMaxSize()) {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(columns),
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(horizontal = 16.dp, vertical = 12.dp),
-                    verticalArrangement = Arrangement.spacedBy(10.dp),
-                    horizontalArrangement = Arrangement.spacedBy(20.dp),
-                ) {
-
-
-                    item {
-                        AppTextField(
-                            value = state.firstName,
-                            onValueChange = { viewModel.sent(GoogleSignUpAction.SetFirstName(it)) },
-                            label = "First name",
-                            modifier = Modifier.fillMaxWidth()
-                                .viewHint(GoogleSignUpScreenHintStep.FIRST_NAME, hintController.currentStep)
-                        )
-                    }
-
-                    item {
-                        AppTextField(
-                            value = state.lastName,
-                            onValueChange = { viewModel.sent(GoogleSignUpAction.SetLastName(it)) },
-                            label = "Last name",
-                            modifier = Modifier.fillMaxWidth()
-                                .viewHint(GoogleSignUpScreenHintStep.LAST_NAME, hintController.currentStep)
-                        )
-                    }
-                    item {
-                        AppTextField(
-                            value = state.password,
-                            onValueChange = { viewModel.sent(GoogleSignUpAction.SetPassword(it)) },
-                            label = "Password",
-                            modifier = Modifier.fillMaxWidth()
-                                .viewHint(GoogleSignUpScreenHintStep.PASSWORD, hintController.currentStep),
-                            isPassword = true,
-                            helperText = "Password must be between 8 and 60 characters long"
-                        )
-                    }
-
-
-                    item {
-                        SingleSelectInput(
-                            value = state.currency,
-                            items = Currency.entries.toList(),
-                            label = "Currency",
-                            toLabel = { it.name },
-                            showNone = false,
-                            noneLabel = "",
-                            onSelect = { viewModel.sent(GoogleSignUpAction.SetCurrency(it ?: Currency.USD)) },
-                            modifier = Modifier.viewHint(
-                                GoogleSignUpScreenHintStep.CURRENCY_SELECTION,
-                                hintController.currentStep
-                            ),
-                            helperText = "Select your preferred currency for transactions and pricing."
-                        )
-
-                    }
-                }
-
-                Row(
-                    modifier = Modifier.fillMaxWidth()
-                        .viewHint(GoogleSignUpScreenHintStep.AGREEMENT_CHECKBOX, hintController.currentStep),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    Checkbox(
-                        checked = state.agreed,
-                        onCheckedChange = { viewModel.sent(GoogleSignUpAction.SetAgreed(it)) },
-                        colors = CheckboxDefaults.colors(
-                            checkedColor = AppTheme.PrimaryColor,
-                            uncheckedColor = AppTheme.PrimaryColor,
-                            checkmarkColor = AppTheme.PrimaryBack
-                        )
-                    )
-                    Text(
-                        text = "I agree to the terms of service",
-                        color = AppTheme.PrimaryColor,
-                        fontSize = 16.sp
-                    )
-                }
-
-                PrimaryButton(
-                    text = "Submit",
-                    onClick = {
-                        viewModel.sent(GoogleSignUpAction.Submit)
-                    },
-                    modifier = Modifier.width(300.dp)
-                        .padding(bottom = 10.dp)
-                        .align(CenterHorizontally)
-                        .viewHint(GoogleSignUpScreenHintStep.SIGN_UP_BUTTON, hintController.currentStep),
-                    enabled = state.agreed
-                )
+            CenteredContainer(maxWidth = rememberInterfaceMaxWidth() * 1.4f) {
+                Content(state, viewModel, hintController)
             }
 
             state.error?.let {
                 ErrorMessageBox(message = it)
             }
         }
+    }
+}
+
+@Composable
+private fun Content(
+    state: GoogleSignUpState,
+    viewModel: GoogleSignUpViewModel,
+    hintController: GoogleSignUpScreenHintController
+) {
+    val columns = if (isNotPhoneFormat()) 2 else 1
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(columns),
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+
+
+            item {
+                AppTextField(
+                    value = state.firstName,
+                    onValueChange = { viewModel.sent(GoogleSignUpAction.SetFirstName(it)) },
+                    label = "First name",
+                    modifier = Modifier.fillMaxWidth()
+                        .viewHint(GoogleSignUpScreenHintStep.FIRST_NAME, hintController.currentStep)
+                )
+            }
+
+            item {
+                AppTextField(
+                    value = state.lastName,
+                    onValueChange = { viewModel.sent(GoogleSignUpAction.SetLastName(it)) },
+                    label = "Last name",
+                    modifier = Modifier.fillMaxWidth()
+                        .viewHint(GoogleSignUpScreenHintStep.LAST_NAME, hintController.currentStep)
+                )
+            }
+            item {
+                AppTextField(
+                    value = state.password,
+                    onValueChange = { viewModel.sent(GoogleSignUpAction.SetPassword(it)) },
+                    label = "Password",
+                    modifier = Modifier.fillMaxWidth()
+                        .viewHint(GoogleSignUpScreenHintStep.PASSWORD, hintController.currentStep),
+                    isPassword = true,
+                    helperText = "Password must be between 8 and 60 characters long"
+                )
+            }
+
+
+            item {
+                SingleSelectInput(
+                    value = state.currency,
+                    items = Currency.entries.toList(),
+                    label = "Currency",
+                    toLabel = { it.name },
+                    showNone = false,
+                    noneLabel = "",
+                    onSelect = { viewModel.sent(GoogleSignUpAction.SetCurrency(it ?: Currency.USD)) },
+                    modifier = Modifier.viewHint(
+                        GoogleSignUpScreenHintStep.CURRENCY_SELECTION,
+                        hintController.currentStep
+                    ),
+                    helperText = "Select your preferred currency for transactions and pricing."
+                )
+
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .viewHint(GoogleSignUpScreenHintStep.AGREEMENT_CHECKBOX, hintController.currentStep),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Checkbox(
+                checked = state.agreed,
+                onCheckedChange = { viewModel.sent(GoogleSignUpAction.SetAgreed(it)) },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = AppTheme.PrimaryColor,
+                    uncheckedColor = AppTheme.PrimaryColor,
+                    checkmarkColor = AppTheme.PrimaryBack
+                )
+            )
+            Text(
+                text = "I agree to the terms of service",
+                color = AppTheme.PrimaryColor,
+                fontSize = 16.sp
+            )
+        }
+
+        PrimaryButton(
+            text = "Submit",
+            onClick = {
+                viewModel.sent(GoogleSignUpAction.Submit)
+            },
+            modifier = Modifier.width(300.dp)
+                .padding(bottom = 10.dp)
+                .align(CenterHorizontally)
+                .viewHint(GoogleSignUpScreenHintStep.SIGN_UP_BUTTON, hintController.currentStep),
+            enabled = state.agreed
+        )
     }
 }
--- composeApp/src/commonMain/kotlin/vm/words/ua/auth/ui/screen/TelegramSignUpScreen.kt
+++ composeApp/src/commonMain/kotlin/vm/words/ua/auth/ui/screen/TelegramSignUpScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,23 +19,23 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.jetbrains.compose.resources.painterResource
 import vm.words.ua.auth.ui.actions.TelegramSignUpAction
 import vm.words.ua.auth.ui.bundles.ConfirmSignBundle
+import vm.words.ua.auth.ui.hints.TelegramSignUpScreenHintController
 import vm.words.ua.auth.ui.hints.TelegramSignUpScreenHintStep
 import vm.words.ua.auth.ui.hints.createTelegramSignUpScreenHintController
+import vm.words.ua.auth.ui.states.TelegramSignUpState
 import vm.words.ua.auth.ui.vms.TelegramSignUpViewModel
 import vm.words.ua.core.domain.models.enums.Currency
 import vm.words.ua.core.ui.AppTheme
 import vm.words.ua.core.ui.components.*
 import vm.words.ua.core.utils.isNotPhoneFormat
+import vm.words.ua.core.utils.rememberInterfaceMaxWidth
 import vm.words.ua.di.rememberInstance
 import vm.words.ua.navigation.Screen
 import vm.words.ua.navigation.SimpleNavController
 import vm.words.ua.utils.hints.ui.components.SimpleHintHost
 import vm.words.ua.utils.hints.ui.utils.viewHint
-import wordsmultiplatform.composeapp.generated.resources.Res
-import wordsmultiplatform.composeapp.generated.resources.info_in_circle
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -72,7 +74,7 @@ fun TelegramSignUpScreen(
                 showBackButton = true,
                 onBackClick = { navController.popBackStack() },
                 showAdditionalButton = true,
-                additionalButtonImage = painterResource(Res.drawable.info_in_circle),
+                additionalButtonVector = Icons.Filled.Info,
                 onAdditionalClick = {
                     navController.navigate(Screen.Policy)
                 },
@@ -80,118 +82,129 @@ fun TelegramSignUpScreen(
                 additionalButtonStepHint = TelegramSignUpScreenHintStep.PRIVACY_POLICY
             )
 
-            val columns = if (isNotPhoneFormat()) 2 else 1
-
-            Column(modifier = Modifier.fillMaxSize()) {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(columns),
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(horizontal = 16.dp, vertical = 12.dp),
-                    verticalArrangement = Arrangement.spacedBy(10.dp),
-                    horizontalArrangement = Arrangement.spacedBy(20.dp),
-                ) {
-                    item {
-                        AppTextField(
-                            value = state.phoneNumber,
-                            onValueChange = { viewModel.sent(TelegramSignUpAction.SetPhoneNumber(it)) },
-                            label = "Phone number",
-                            modifier = Modifier.fillMaxWidth()
-                                .viewHint(TelegramSignUpScreenHintStep.PHONE_NUMBER, hintController.currentStep),
-                            helperText = "Include country code, e.g., 11234567890, 3801234567890"
-                        )
-                    }
-
-                    item {
-                        AppTextField(
-                            value = state.password,
-                            onValueChange = { viewModel.sent(TelegramSignUpAction.SetPassword(it)) },
-                            label = "Password",
-                            modifier = Modifier.fillMaxWidth()
-                                .viewHint(TelegramSignUpScreenHintStep.PASSWORD, hintController.currentStep),
-                            isPassword = true,
-                            helperText = "Password must be between 8 and 60 characters long"
-                        )
-                    }
-
-                    item {
-                        AppTextField(
-                            value = state.firstName,
-                            onValueChange = { viewModel.sent(TelegramSignUpAction.SetFirstName(it)) },
-                            label = "First name",
-                            modifier = Modifier.fillMaxWidth()
-                                .viewHint(TelegramSignUpScreenHintStep.FIRST_NAME, hintController.currentStep)
-                        )
-                    }
-
-                    item {
-                        AppTextField(
-                            value = state.lastName,
-                            onValueChange = { viewModel.sent(TelegramSignUpAction.SetLastName(it)) },
-                            label = "Last name",
-                            modifier = Modifier.fillMaxWidth()
-                                .viewHint(TelegramSignUpScreenHintStep.LAST_NAME, hintController.currentStep)
-                        )
-                    }
-
-
-                    item {
-                        SingleSelectInput(
-                            value = state.currency,
-                            items = Currency.entries.toList(),
-                            label = "Currency",
-                            toLabel = { it.name },
-                            showNone = false,
-                            noneLabel = "",
-                            onSelect = { viewModel.sent(TelegramSignUpAction.SetCurrency(it ?: Currency.USD)) },
-                            modifier = Modifier.viewHint(
-                                TelegramSignUpScreenHintStep.CURRENCY_SELECTION,
-                                hintController.currentStep
-                            ),
-                            helperText = "Select your preferred currency for transactions and pricing."
-                        )
-
-                    }
-                }
-
-                Row(
-                    modifier = Modifier.fillMaxWidth()
-                        .viewHint(TelegramSignUpScreenHintStep.AGREEMENT_CHECKBOX, hintController.currentStep),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    Checkbox(
-                        checked = state.agreed,
-                        onCheckedChange = { viewModel.sent(TelegramSignUpAction.SetAgreed(it)) },
-                        colors = CheckboxDefaults.colors(
-                            checkedColor = AppTheme.PrimaryColor,
-                            uncheckedColor = AppTheme.PrimaryColor,
-                            checkmarkColor = AppTheme.PrimaryBack
-                        )
-                    )
-                    Text(
-                        text = "I agree to the terms of service",
-                        color = AppTheme.PrimaryColor,
-                        fontSize = 16.sp
-                    )
-                }
-
-                PrimaryButton(
-                    text = "Submit",
-                    onClick = {
-                        viewModel.sent(TelegramSignUpAction.Submit)
-                    },
-                    modifier = Modifier.width(300.dp)
-                        .padding(bottom = 10.dp)
-                        .align(CenterHorizontally)
-                        .viewHint(TelegramSignUpScreenHintStep.SIGN_UP_BUTTON, hintController.currentStep),
-                    enabled = state.agreed
-                )
+            CenteredContainer(maxWidth = rememberInterfaceMaxWidth() * 1.4f) {
+                Content(state, viewModel, hintController)
             }
 
             state.error?.let {
                 ErrorMessageBox(message = it)
             }
         }
+    }
+}
+
+@Composable
+private fun Content(
+    state: TelegramSignUpState,
+    viewModel: TelegramSignUpViewModel,
+    hintController: TelegramSignUpScreenHintController
+) {
+    val columns = if (isNotPhoneFormat()) 2 else 1
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(columns),
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            item {
+                AppTextField(
+                    value = state.phoneNumber,
+                    onValueChange = { viewModel.sent(TelegramSignUpAction.SetPhoneNumber(it)) },
+                    label = "Phone number",
+                    modifier = Modifier.fillMaxWidth()
+                        .viewHint(TelegramSignUpScreenHintStep.PHONE_NUMBER, hintController.currentStep),
+                    helperText = "Include country code, e.g., 11234567890, 3801234567890"
+                )
+            }
+
+            item {
+                AppTextField(
+                    value = state.password,
+                    onValueChange = { viewModel.sent(TelegramSignUpAction.SetPassword(it)) },
+                    label = "Password",
+                    modifier = Modifier.fillMaxWidth()
+                        .viewHint(TelegramSignUpScreenHintStep.PASSWORD, hintController.currentStep),
+                    isPassword = true,
+                    helperText = "Password must be between 8 and 60 characters long"
+                )
+            }
+
+            item {
+                AppTextField(
+                    value = state.firstName,
+                    onValueChange = { viewModel.sent(TelegramSignUpAction.SetFirstName(it)) },
+                    label = "First name",
+                    modifier = Modifier.fillMaxWidth()
+                        .viewHint(TelegramSignUpScreenHintStep.FIRST_NAME, hintController.currentStep)
+                )
+            }
+
+            item {
+                AppTextField(
+                    value = state.lastName,
+                    onValueChange = { viewModel.sent(TelegramSignUpAction.SetLastName(it)) },
+                    label = "Last name",
+                    modifier = Modifier.fillMaxWidth()
+                        .viewHint(TelegramSignUpScreenHintStep.LAST_NAME, hintController.currentStep)
+                )
+            }
+
+
+            item {
+                SingleSelectInput(
+                    value = state.currency,
+                    items = Currency.entries.toList(),
+                    label = "Currency",
+                    toLabel = { it.name },
+                    showNone = false,
+                    noneLabel = "",
+                    onSelect = { viewModel.sent(TelegramSignUpAction.SetCurrency(it ?: Currency.USD)) },
+                    modifier = Modifier.viewHint(
+                        TelegramSignUpScreenHintStep.CURRENCY_SELECTION,
+                        hintController.currentStep
+                    ),
+                    helperText = "Select your preferred currency for transactions and pricing."
+                )
+
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .viewHint(TelegramSignUpScreenHintStep.AGREEMENT_CHECKBOX, hintController.currentStep),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Checkbox(
+                checked = state.agreed,
+                onCheckedChange = { viewModel.sent(TelegramSignUpAction.SetAgreed(it)) },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = AppTheme.PrimaryColor,
+                    uncheckedColor = AppTheme.PrimaryColor,
+                    checkmarkColor = AppTheme.PrimaryBack
+                )
+            )
+            Text(
+                text = "I agree to the terms of service",
+                color = AppTheme.PrimaryColor,
+                fontSize = 16.sp
+            )
+        }
+
+        PrimaryButton(
+            text = "Submit",
+            onClick = {
+                viewModel.sent(TelegramSignUpAction.Submit)
+            },
+            modifier = Modifier.width(300.dp)
+                .padding(bottom = 10.dp)
+                .align(CenterHorizontally)
+                .viewHint(TelegramSignUpScreenHintStep.SIGN_UP_BUTTON, hintController.currentStep),
+            enabled = state.agreed
+        )
     }
 }
